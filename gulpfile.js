ec29df189d8cc0f19fde0b0ebfd1b4796c88bf60
bengbengku <anggiatbenget70@gmail.com>
2020-10-07 23:07:18 +0700

    ADD: Menambah minify kompress di gulpfile-memperkecil-ukuran

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
   return gulp.src('./stylesheets/new-style.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./build'));
 });
  
